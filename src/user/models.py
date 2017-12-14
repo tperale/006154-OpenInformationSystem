6c38e24adade1d5c209c12176652a5e22e8db8db
Myxfall <maximilienromain96@hotmail.com>
2017-12-14 14:51:40 +0100

    datas for user models

--- src/user/models.py
+++ src/user/models.py
@@ -23,3 +23,36 @@ class Purchase(models.Model):
 class UserBookPurchase(models.Model):
     from_purchase = models.ForeignKey('Purchase', on_delete=models.CASCADE)
     ebook = models.ForeignKey('ebook.ebook', on_delete=models.CASCADE)
+
+
+# ----- DATAS INSERTION -----
+Author(first_name = "J. R. R.", last_name = "Tolkien").save()
+Author(first_name = "J. K.", last_name = "Rowling").save()
+
+Customer(first_name = "Max", last_name = "Romain", email = "maxromai@ulb.ac.be", password = "12345")
+Customer(first_name = "Thomas", last_name = "Peral", email = "tPeral@ulb.ac.be", password = "54321")
+Customer(first_name = "Felipe", last_name = "Rojas", email = "frojas@vub.be", password = "rojas")
+Customer(first_name = "Sherik", last_name = "Lehal", email = "slehal@vub.be", password = "lehal")
+
+#Purchase made by Max
+Purchase(done = "14/12/17", by = 1)
+Purchase(done = "10/12/17", by = 1)
+Purchase(done = "02/06/17", by = 1)
+
+#TODO: ebook ForeignKey
+UserBookPurchase(from_purchase = 1, ebook = )
+UserBookPurchase(from_purchase = 2, ebook = )
+UserBookPurchase(from_purchase = 3, ebook = )
+
+#Purchase made by Thomas
+Purchase(done = "10/12/17", by = 2)
+Purchase(done = "01/01/17", by = 2)
+
+UserBookPurchase(from_purchase = 4, ebook = )
+UserBookPurchase(from_purchase = 5, ebook = )
+
+#Purchase made by Felipe
+Purchase(done = "05/05/16", by = 3)
+UserBookPurchase(from_purchase = 6, ebook = )
+
+#0 books for Sherik
