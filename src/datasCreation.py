006e610c1ca02f9956ea5062880132639a2d9228
Myxfall <maximilienromain96@hotmail.com>
2017-12-14 18:57:07 +0100

    correction datasCreation.py

--- src/datasCreation.py
+++ src/datasCreation.py
@@ -28,7 +28,7 @@ publisher4.save()
 #eBooks
 eBook1 = eBook(isbn = 9781491957615,title='Python for Data Analysis',version = 1,release='2017-09-01 00:00:00',author3,publisher1)
 eBook2 = eBook(isbn = 9781405519229,title='THE CASUAL VACANCY',version = 1,release='2012-09-01 00:00:00',author2,publisher2)
-eBook3 = eBook(isbn = 9780007322596,title='The Lord of the Rings: The Fellowship of the Ring, The Two Towers, The Return of the King',version = 1,release='2009-05-01 00:00:00',author1,publisher3).save()
+eBook3 = eBook(isbn = 9780007322596,title='The Lord of the Rings: The Fellowship of the Ring, The Two Towers, The Return of the King',version = 1,release='2009-05-01 00:00:00',author1,publisher3)
 
 eBook1.save()
 eBook2.save()
@@ -45,17 +45,14 @@ customer2.save()
 customer3.save()
 customer4.save()
 
-
-
 #Purchase made by Max
 purchase1 = Purchase(done = "14/12/17", by = customer1)
 purchase2 = Purchase(done = "10/12/17", by = customer1)
 purchase3 = Purchase(done = "02/06/17", by = customer1)
 
-#TODO: ebook ForeignKey
-Customer2Purchase1 = UserBookPurchase(from_purchase = purchase1, ebook = eBook1)
-Customer2Purchase2 = UserBookPurchase(from_purchase = purchase2, ebook = eBook2)
-Customer2Purchase3 = UserBookPurchase(from_purchase = purchase3, ebook = eBook3)
+Customer1Purchase1 = UserBookPurchase(from_purchase = purchase1, ebook = eBook1)
+Customer1Purchase2 = UserBookPurchase(from_purchase = purchase2, ebook = eBook2)
+Customer1Purchase3 = UserBookPurchase(from_purchase = purchase3, ebook = eBook3)
 
 Customer1Purchase1.save()
 Customer1Purchase2.save()
@@ -68,8 +65,8 @@ purchase5 = Purchase(done = "01/01/17", by = customer2)
 purchase4.save()
 purchase5.save()
 
-Customer2Purchase1 = UserBookPurchase(from_purchase = purchase4, ebook = eBook2).save()
-Customer2Purchase2 = UserBookPurchase(from_purchase = purchase5, ebook = eBook3).save()
+Customer2Purchase1 = UserBookPurchase(from_purchase = purchase4, ebook = eBook2)
+Customer2Purchase2 = UserBookPurchase(from_purchase = purchase5, ebook = eBook3)
 
 Customer2Purchase1.save()
 Customer2Purchase2.save()
@@ -77,7 +74,7 @@ Customer2Purchase2.save()
 #Purchase made by Felipe
 purchase6 = Purchase(done = "05/05/16", by = customer3)
 
-Customer3Purchase1 = UserBookPurchase(from_purchase = purchase6, ebook = eBook1).save()
+Customer3Purchase1 = UserBookPurchase(from_purchase = purchase6, ebook = eBook1)
 
-Customer3Purchase1
+Customer3Purchase1.save()
 #0 books for Sherik
