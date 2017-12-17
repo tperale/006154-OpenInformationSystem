8014510b7f41ccf31bf35b194254fedb590ff81b
Thomas Perale <thomas.perale@openmailbox.org>
2017-12-17 21:05:14 +0100

    [add] profile

--- src/user/urls.py
+++ src/user/urls.py
@@ -5,5 +5,6 @@ from user import views
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html', 'next_page': '/'}, name='logout'),
+    url(r'^profile/$', views.profile, name='profile'),
     url(r'^signup/$', views.signup, name='signup'),
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
 def signup(request):
     if request.method == 'POST':
@@ -15,3 +17,48 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+@login_required
+def profile(request):
+    user = request.user
+    message = ""
+
+    current_info = {
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+    }
+
+    # return HttpResponse("this is the user profile page for username '%s'. update first, last, email, username, and password here." % request.user.username)
+    if request.method == 'POST':
+
+        # create a form instance and populate it with data from the request
+        form = ProfileForm(request.POST)
+
+        if form.is_valid():
+
+            if form.cleaned_data['username'] != user.username:
+                user.username = form.cleaned_data['username']
+
+            if form.cleaned_data['first_name'] != user.first_name:
+                user.first_name = form.cleaned_data['first_name']
+
+            if form.cleaned_data['last_name'] != user.last_name:
+                user.last_name = form.cleaned_data['last_name']
+
+            if form.cleaned_data['email'] != user.email:
+                user.email = form.cleaned_data['email']
+
+            if form.cleaned_data['password'] != user.password and form.cleaned_data['password'] != "":
+                user.set_password(form.cleaned_data['password'])
+
+            user.save()
+            message += "Profile updated."
+
+            return render(request, 'profile.html', {'form': form, 'message': message})
+
+    else:
+        form = ProfileForm(current_info)
+
+    return render(request, 'profile.html', {'form': form})
