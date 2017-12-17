9148957b48ce46ee6893f2b2cd5efcfd732d9603
Thomas Perale <thomas.perale@openmailbox.org>
2017-12-17 17:54:56 +0100

    [add] Login and register

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('home.urls')),
+    url(r'^user/', include('user.urls')),
     url(r'^admin/', admin.site.urls),
     # url(r'^ebook/', include('ebook.urls')),
 ]
---
+++ src/user/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from user import views
+
+urlpatterns = [
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html', 'next_page': '/'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+]
