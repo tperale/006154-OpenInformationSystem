477a0392818a0617be76fec0b750aad988d9a027
Thomas Perale <thomas.perale@openmailbox.org>
2017-12-18 10:11:20 +0100

    [enh] book recommendation on landing page

--- src/home/views.py
+++ src/home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from ebook.models import Ebook
+from random import sample
 
 class Welcome(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
-        context = super(Welcome, self).get_context_data(**kwargs)
-        return context
+        books = Ebook.objects.all()
+        return {'recommended': sample(list(books), 3)}
